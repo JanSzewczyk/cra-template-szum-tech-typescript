248a93cc65354d7cca61c1014f82c1957d561671
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-05-30 17:07:25 +0200

    feat(template): create `React` app template

---
+++ template/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+// TODO refactor after eslint config package update
+// eslint-disable-next-line require-jsdoc
+function App() {
+  return (
+    <div className={"App"}>
+      <header className={"App-header"}>
+        <img src={"./logo.svg"} className={"App-logo"} alt={"logo"} />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className={"App-link"} href={"https://reactjs.org"} target={"_blank"} rel={"noopener noreferrer"}>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
